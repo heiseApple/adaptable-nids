e436f68e54855afc4ab865b9d52ddf101e423f07
Davide <davide.dimonda@unina.it>
2025-04-03 14:53:14 +0100

    Update save_feats.py

--- script/save_feats.py
+++ script/save_feats.py
@@ -7,17 +7,14 @@ if src_path not in sys.path:
     sys.path.append(src_path)
 
 import json
+import torch
 import numpy as np
 from tqdm import tqdm
 from argparse import ArgumentParser
 from types import SimpleNamespace
 
-from sklearn.model_selection import train_test_split
 from sklearn.manifold import TSNE
 from sklearn.metrics import silhouette_score
-
-import torch
-from torch.utils.data import DataLoader, TensorDataset
 
 from data.data_manager import DataManager
 from approach.approach_factory import get_approach
@@ -36,7 +33,10 @@ def embed_dataset(approach, loader, desc):
             embeddings.append(batch_emb)
             labels.append(batch_y)
 
-    return torch.cat(embeddings, dim=0), torch.cat(labels, dim=0)
+    return (
+        torch.cat(embeddings, dim=0).detach().cpu().numpy(), 
+        torch.cat(labels, dim=0).detach().cpu().numpy()
+    )
 
 
 def main():
@@ -44,12 +44,12 @@ def main():
     parser = ArgumentParser(conflict_handler='resolve', add_help=True)
     parser.add_argument('-a', '--args-path', type=str, default=None,
                         help='Path to dict_args.json')
-    parser.add_argument('-w', '--weights-path', type=str, default=None,
-                        help='Path to the .pt file containing the network weights')
-    parser.add_argument('-d', '--data-percent', type=float, default=1.0,
-                        help='Percentage of data to use (stratified sampling)')
+    parser.add_argument('-c', '--ckpt-path', type=str, default=None,
+                        help='Path to the .pt file containing the state of an approach')
     parser.add_argument('-o', '--output-fn', type=str, default='coords_2d.npz',
                         help='Output filename')
+    parser.add_argument('-t', '--tsne', action='store_true', help='Compute TSNE embeddings')
+    parser.add_argument('-s', '--silscore', action='store_true', help='Compute silhouette score')
     script_args = parser.parse_args()
 
     # Load experiment arguments
@@ -64,61 +64,22 @@ def main():
 
     # Prepare data
     dm = DataManager(exp_args)
-    dm.load_datasets()
+    src_splits, trg_splits, _ = dm.get_dataset_splits()
     
-    # Apply stratified sampling if needed
-    if script_args.data_percent < 1.0:
-        src_data = dm.src_dataset['data']
-        src_labels = dm.src_dataset['labels']
-        src_data, _, src_labels, _ = train_test_split(
-            src_data, src_labels, 
-            train_size=script_args.data_percent,
-            stratify=src_labels,
-            random_state=0
-        )
-        dm.src_dataset['data'] = src_data
-        dm.src_dataset['labels'] = src_labels
-
-        trg_data = dm.trg_dataset['data']
-        trg_labels = dm.trg_dataset['labels']
-        trg_data, _, trg_labels, _ = train_test_split(
-            trg_data, trg_labels,
-            train_size=script_args.data_percent,
-            stratify=trg_labels,
-            random_state=0
-        )
-        dm.trg_dataset['data'] = trg_data
-        dm.trg_dataset['labels'] = trg_labels
-
-    src_loader = DataLoader(
-        TensorDataset(
-            torch.from_numpy(dm.src_dataset['data']).float(),
-            torch.from_numpy(dm.src_dataset['labels']).float()
-        ),
-        batch_size=exp_args.batch_size,
-        shuffle=False,
-        num_workers=0,
-        pin_memory=False,
-    )
-    trg_loader = DataLoader(
-        TensorDataset(
-            torch.from_numpy(dm.trg_dataset['data']).float(),
-            torch.from_numpy(dm.trg_dataset['labels']).float()
-        ),
-        batch_size=exp_args.batch_size,
-        shuffle=False,
-        num_workers=0,
-        pin_memory=False,
-    )
-
+    src_datamodule = src_splits.get_datamodule(**vars(exp_args))
+    trg_datamodule = trg_splits.get_datamodule(**vars(exp_args))
+    
+    src_loader = src_datamodule.get_test_data()
+    trg_loader = trg_datamodule.get_test_data()
+    
     # Load the approach and network weights
     approach = get_approach(
         approach_name=exp_args.approach,
         fs_task=exp_args.k is not None,
         **vars(exp_args)
     )
-    approach.net.load_weights(script_args.weights_path)
-    print(f'Loaded network weights from {script_args.weights_path}')
+    approach.load_checkpoint(script_args.ckpt_path) 
+    print(f'Loaded approach state from {script_args.ckpt_path}')
 
     # Embed datasets
     src_embeddings, src_labels = embed_dataset(approach, src_loader, 'Embedding SRC dataset')
@@ -132,22 +93,24 @@ def main():
     src_mask[:len(src_labels)] = True
 
     # Apply TSNE
-    reducer = TSNE(n_components=2, verbose=1)
-    coords_2d = reducer.fit_transform(all_embeddings)
+    if script_args.tsne:
+        reducer = TSNE(n_components=2, verbose=1)
+        coords_2d = reducer.fit_transform(all_embeddings)
     
     # Compute silhouette score
-    binary_labels = np.where(all_labels % 2 == 0, 0, 1)
-    sil_score = silhouette_score(all_embeddings, binary_labels)
+    if script_args.silscore:
+        binary_labels = np.where(all_labels % 2 == 0, 0, 1)
+        sil_score = silhouette_score(all_embeddings, binary_labels)
     
     # Save embeddings and labels to npz file
     np.savez(
         script_args.output_fn, 
-        coords_2d=coords_2d, 
-        src_labels=src_labels.detach().cpu().numpy(), 
-        trg_labels=trg_labels.detach().cpu().numpy(),
-        src_embeddings=src_embeddings.detach().cpu().numpy(),
-        trg_embeddings=trg_embeddings.detach().cpu().numpy(),
-        sil = sil_score,
+        coords_2d=coords_2d if script_args.tsne else None, 
+        src_labels=src_labels, 
+        trg_labels=trg_labels,
+        src_embeddings=src_embeddings,
+        trg_embeddings=trg_embeddings,
+        sil = sil_score if script_args.silscore else None,
     )
 
 if __name__ == '__main__':
